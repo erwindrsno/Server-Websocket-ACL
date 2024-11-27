459227aa38737531303f60bf18678d816f165826
Erwin Darsono <erwindarsono08@gmail.com>
2024-11-27 16:15:31 +0700

    reconstruct everything

---
+++ run_server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd target
+java -jar ws-server-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/ws/server/SimpleServer.java
+++ src/main/java/ws/server/SimpleServer.java
@@ -2,8 +2,12 @@ package ws.server;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
@@ -15,6 +19,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SimpleServer extends WebSocketServer {
     final int CHUNK_SIZE = 10240;
     Logger logger = LoggerFactory.getLogger(SimpleServer.class);
+    Scanner sc = new Scanner(System.in);
+    private final ConcurrentHashMap<String, WebSocket> clients = new ConcurrentHashMap<>();
 
     public SimpleServer(InetSocketAddress address) {
         super(address);
@@ -24,6 +30,69 @@ public class SimpleServer extends WebSocketServer {
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         logger.info("Client connected from " + conn.getRemoteSocketAddress().getAddress());
 
+        //TODO: Temporary storing UUID as key
+        //IDEA: STORING LAB NAME 
+        //e.g: LAB01-08, and etc
+        String clientId = UUID.randomUUID().toString();
+        clients.put(clientId, conn);
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println(
+                "closed " + conn.getRemoteSocketAddress().getAddress() + " with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        // System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
+        // logger.info("Received Pong from : " + conn.getRemoteSocketAddress());
+        if(message.equals("PONG")){
+            logger.info("Received Pong from : " + conn.getRemoteSocketAddress().getAddress());
+        }
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress() + ":" + ex);
+    }
+
+    @Override
+    public void onStart() {
+        String myIP = "";
+        try{
+            InetAddress me = InetAddress.getLocalHost();
+            myIP += me.getHostAddress();
+        } catch(Exception e){
+            logger.error("Error getting localhost IP address");
+            e.printStackTrace();
+        }
+        logger.info("Server started at ws://" + myIP + ":8887");
+
+        new Thread(() -> {
+            logger.info("Command receiver thread started");
+            while(true){
+                System.out.print("Command : ");
+                String command = sc.next();
+                switch(command){
+                    case "ping":
+                        broadcast("PING");
+                        break;
+
+                    case "send":
+                        sendFile();
+                        break;
+                }
+            }
+        }).start();
+    }
+
+    public void sendFile() {
         File file = new File("T06xxyyy.zip");
 
         if (!file.exists()) {
@@ -39,9 +108,7 @@ public class SimpleServer extends WebSocketServer {
                 logger.info("Sending 10240");
                 ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, bytesRead);
 
-                conn.send(byteBuffer);
-
-                // conn.sendFragmentedFrame(Opcode.BINARY, byteBuffer, isLastChunk);
+                broadcast(byteBuffer);
             }
             logger.info("File sent");
         } catch (Exception e) {
@@ -50,43 +117,19 @@ public class SimpleServer extends WebSocketServer {
         }
 
         try {
-            logger.info("Sending file name and user name");
-            String fileName = file.getName();
-            UserFile userFile = new UserFile("car", fileName);
+            //TODO : NEED TO REHANDLE
 
-            ObjectMapper mapper = new ObjectMapper();
-            String json = mapper.writeValueAsString(userFile);
-            conn.send(json);
-            logger.info("File name and user name sent");
+            // logger.info("Sending file name and user name");
+            // String fileName = file.getName();
+            // UserFile userFile = new UserFile("car", fileName);
+
+            // ObjectMapper mapper = new ObjectMapper();
+            // String json = mapper.writeValueAsString(userFile);
+            // broadcast(json);
+            // logger.info("File name and user name sent");
         } catch (Exception e) {
             logger.error("error at sending file name and user name");
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println(
-                "closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-        System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, ByteBuffer message) {
-        System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress() + ":" + ex);
-    }
-
-    @Override
-    public void onStart() {
-        logger.info("Server started at ws://localhost:8887");
     }
 }
