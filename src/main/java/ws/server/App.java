68c1768f8701d18a4a030d3f60e0903e68eed2c6
Erwin Darsono <erwindarsono08@gmail.com>
2024-12-12 19:31:40 +0700

    change ip

--- src/main/java/ws/server/App.java
+++ src/main/java/ws/server/App.java
@@ -6,7 +6,7 @@ import org.java_websocket.server.WebSocketServer;
 
 public class App {
     public static void main(String[] args) {
-        String host = "192.168.0.102";
+        String host = "10.100.70.60";
         int port = 8887;
 
         WebSocketServer server = new Server(new InetSocketAddress(host, port));
